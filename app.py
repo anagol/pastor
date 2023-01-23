634c1870e64e5aade75fce89846cfb4b6251d6e2
anagol <golosmoy69@gmail.com>
2023-01-23 19:02:40 +0300

    [P-07]

--- app.py
+++ app.py
@@ -8,7 +8,20 @@ import psycopg2
 import locale
 import requests
 from bs4 import BeautifulSoup as bs
+from flask_paginate import Pagination, get_page_args
 import lxml
+
+# ---------------Парсер-----------
+url_1 = 'https://eparhia992.by/component/search/?searchword=%D0%BA%D1%83%D1%88%D0%BD%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%D1%87&searchphrase=all&limit=0'
+
+response = requests.get(url_1)
+soup = bs(response.text, "html.parser")
+names = soup.find_all('a')
+pub_date = soup.find_all(class_='result-created')
+name_result = [x for x in names if 'item' in x.get('href')]
+string_result = min(len(pub_date), len(name_result))
+# ----------------------------------------------------------
+
 
 locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
@@ -52,8 +65,8 @@ class News(db.Model):
         return '<News %r>' % self.news_headline
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
 @login_manager.user_loader
@@ -84,7 +97,7 @@ def about():
 def news():
     news_total = News.query.all()
     return render_template('news.html', title='Новости храма', news_total=news_total, names=names,
-                           )
+                           pub_date=pub_date, name_result=name_result, string_result=string_result)
 
 
 # ----------------Создание новости-----------------------------
@@ -221,19 +234,6 @@ def logout():
 def error_401():
     return render_template('401.html', title='ОШИБКА 401')
 
-
-# ---------------Парсер-----------
-url_1 = 'https://eparhia992.by/component/search/?searchword=%D0%BA%D1%83%D1%88%D0%BD%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%D1%87&searchphrase=all&limit=0'
-
-response = requests.get(url_1)
-soup = bs(response.text, "html.parser")
-names = soup.find_all('a')
-
-for name in names:
-    if 'item' in name.get('href'):
-        result_text = name.text
-        result_url = 'https://eparhia992.by' + name.get('href')
-# ----------------------------------------------------------
 
 if __name__ == '__main__':
     app.run()
