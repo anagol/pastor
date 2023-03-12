1619349a87ac7d71b0eca2e02ca29f92611d2d97
anago <golosnoy69@gmail.com>
2023-03-12 11:58:37 +0300

    [P-111]

--- app.py
+++ app.py
@@ -27,16 +27,16 @@ bootstrap = Bootstrap5(app)
 
 
 
-# class Parser:
-#     url = 'https://eparhia992.by/component/search/?searchword=%D0%BA%D1%83%D1%88%D0%BD%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%' \
-#           'D1%87&searchphrase=all&limit=0'
-#
-#     response = requests.get(url)
-#     soup = bs(response.text, "html.parser")
-#     names = soup.find_all('a')
-#     pub_date = soup.find_all(class_='result-created')
-#     name_result = [x for x in names if 'item' in x.get('href')]
-#     string_result = min(len(pub_date), len(name_result))
+class Parser:
+    url = 'https://eparhia992.by/component/search/?searchword=%D0%BA%D1%83%D1%88%D0%BD%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%' \
+          'D1%87&searchphrase=all&limit=0'
+
+    response = requests.get(url)
+    soup = bs(response.text, "html.parser")
+    names = soup.find_all('a')
+    pub_date = soup.find_all(class_='result-created')
+    name_result = [x for x in names if 'item' in x.get('href')]
+    string_result = min(len(pub_date), len(name_result))
 
 
 class User(UserMixin, db.Model):
