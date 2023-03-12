901e037149db2eab9b93ef61b86327ee8f4a3395
anago <golosnoy69@gmail.com>
2023-03-12 11:31:18 +0300

    [P-17]

--- app.py
+++ app.py
@@ -17,25 +17,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'anatolihalasny1969'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:nataha@localhost:5432/pastor"
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
+# db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 bootstrap = Bootstrap5(app)
 
 
-class Parser:
-    url = 'https://eparhia992.by/component/search/?searchword=%D0%BA%D1%83%D1%88%D0%BD%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%' \
-          'D1%87&searchphrase=all&limit=0'
-
-    response = requests.get(url)
-    soup = bs(response.text, "html.parser")
-    names = soup.find_all('a')
-    pub_date = soup.find_all(class_='result-created')
-    name_result = [x for x in names if 'item' in x.get('href')]
-    string_result = min(len(pub_date), len(name_result))
+# class Parser:
+#     url = 'https://eparhia992.by/component/search/?searchword=%D0%BA%D1%83%D1%88%D0%BD%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%' \
+#           'D1%87&searchphrase=all&limit=0'
+#
+#     response = requests.get(url)
+#     soup = bs(response.text, "html.parser")
+#     names = soup.find_all('a')
+#     pub_date = soup.find_all(class_='result-created')
+#     name_result = [x for x in names if 'item' in x.get('href')]
+#     string_result = min(len(pub_date), len(name_result))
 
 
 class User(UserMixin, db.Model):
